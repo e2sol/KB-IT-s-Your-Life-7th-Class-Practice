5423973461e46781d51b2ab62adaa920ee377d92
Student <student@example.com>
2026-04-06 16:56:36 +0900

    pinia 상태관리 학습: 프로젝트 초기 설정 및 기본 예제 추가

---
+++ 03_VueJS/01_basic/12_pinia상태관리/pinia-app/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Like from '@/components/Like.vue';
+
+// 01_basic
+import { useLikeStore } from '@/stores/01_basic/like.js';
+const likeStore = useLikeStore();
+</script>
+
+<template>
+  <div>
+    <h1>Pinia 상태 관리 예제</h1>
+    <h2>좋아요 수 : {{ likeStore.like }}</h2>
+    <h2>좋아요 수 5배 증가 : {{ likeStore.fivetimes }}</h2>
+    <Like />
+  </div>
+</template>
+
+<style scoped></style>
