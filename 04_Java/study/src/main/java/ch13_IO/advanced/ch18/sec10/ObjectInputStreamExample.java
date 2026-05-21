34b6f42124d44bba64f215509c7fb07091a8e99a
Hyeji Han <hj4094@naver.com>
2026-05-21 20:45:31 +0900

    5/21 IO 심화실습 완료

--- 04_Java/study/src/main/java/ch13_IO/advanced/ch18/sec10/ObjectInputStreamExample.java
+++ 04_Java/study/src/main/java/ch13_IO/advanced/ch18/sec10/ObjectInputStreamExample.java
@@ -1,7 +1,23 @@
 package main.java.ch13_IO.advanced.ch18.sec10;
 
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.Arrays;
+
 public class ObjectInputStreamExample {
     public static void main(String[] args) throws Exception{
+        FileInputStream fis = new FileInputStream("C:/Temp/object.dat");
+        ObjectInputStream ois = new ObjectInputStream(fis);
 
+        Member m2 = (Member) ois.readObject();
+        Product p2 = (Product) ois.readObject();
+        int[] arr2 = (int[]) ois.readObject();
+
+        System.out.println("m2:\t" + m2.toString());
+        System.out.println("p2:\t" + p2.toString());
+        System.out.println("arr2:\t" + Arrays.toString(arr2));
+
+        ois.close();
+        fis.close();
     }
 }
