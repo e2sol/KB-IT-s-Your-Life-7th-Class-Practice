4ec0464d8dc4d4f0a99e4a32b4b19aefd644613d
Hyeji Han <hj4094@naver.com>
2026-05-29 09:28:52 +0900

    5/28 MySQL 테이블, 뷰 기본 및 심화 실습 완료, 오후 수업 실습코드

--- 05_DB/01_MySQL/Class_Explan/02_dql_select2.sql
+++ 05_DB/01_MySQL/Class_Explan/02_dql_select2.sql
@@ -112,45 +112,59 @@ from tbl_menu;
 
 # sum(컬럼)
 # - null이 아닌 컬럼의 합
+select * from tbl_menu;
 
+-- 메뉴 가격 총 합 구하기
+select sum(menu_price) from tbl_menu;
 
-
-
-
+-- 카테고리 코드가 10번인 메뉴들의 가격 합계 조회
+select sum(menu_price) from tbl_menu where category_code = 10;
 
 # avg(컬럼) -> 평균값
 # - null인 컬럼은 제외한 평균값
 
+select avg(menu_price) from tbl_menu;
 
+select truncate(avg(menu_price), 0) from tbl_menu;
 
 # count(컬럼) -> 개수
 # - null인 컬럼은 제외하고 개수 집계
-
-
+select
+    count(*), -- * (모든 컬럼 = 행)
+    count(menu_price) -- null인 컬럼 제외
+from tbl_menu;
 
 # 카테고리코드가 null이 아닌 행 개수 조회
+select count(category_code) from tbl_menu;
 
-
-
+select count(*) from tbl_menu where category_code is not null;
 
 # max/min
 # - 숫자/문자열/날짜시간에 대해 최대/최소값을 반환
-
-
-
+select max(menu_price), min(menu_price), max(menu_name), min(menu_name) from tbl_menu;
 
 -- =====================================================
 # group by 구문
 # - 특정컬럼을 기준으로 grouping을 수행
 # - 그룹함수와 함께 사용
-
+select
+    category_code, -- 그룹핑 기준 컬럼
+    count(*),
+    avg(menu_price)
+from tbl_menu
+group by category_code; -- category_code값이 같은 행끼리 그룹핑 처리(null도 하나의 그룹으로 처리)
 
 # 주문가능 여부에 따른 메뉴개수 집계
-
-
+select orderable_status, count(*)
+from tbl_menu group by orderable_status;
 
 # 두개이상 컬럼에 대해서 그룹핑 가능
-
+select category_code, orderable_status, count(*)
+from tbl_menu
+group by
+    # category_code, orderable_status
+    orderable_status, category_code -- 상관 없음
+order by category_code;
 
 
 # having 조건절
@@ -158,13 +172,28 @@ from tbl_menu;
 # - where절과 달리 그룹함수 작성가능하다.
 
 # 카테고리별 메뉴개수가 2개이상인 카테고리(카테고리명, 개수) 조회
-
+select category_name, count(*)
+from tbl_category
+join tbl_menu on tbl_menu.category_code = tbl_category.category_code
+group by category_name
+having count(*) >= 2;
 
 
 # 카테고리별 메뉴 개수 조회 (단, category_code가 NULL인 그룹 제외)
 # 일반적인 조건절
+select category_code, count(*)
+from tbl_menu
+group by category_code
+having category_code is not null; -- 일반 컬럼 조건은 WHERE가 더 효율적
 
+-- 이유
+-- HAVING : 그룹 생성 후 null 제거
+-- WHERE : 그룹 생성 전 null 제거
 
-
-
-
+select category_name, count(*) cnt -- 5
+from tbl_menu m
+join tbl_category on m.category_code = tbl_category.category_code -- 1
+where m.category_code is not null -- 2
+group by m.category_code -- 3
+having cnt > 2 -- 4
+order by cnt; -- 6
