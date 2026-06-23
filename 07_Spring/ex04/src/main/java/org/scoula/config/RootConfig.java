6ea5a2c097fcc1a8148a15a29962af48200f1b64
Hyeji Han <hj4094@naver.com>
2026-06-23 14:01:27 +0900

    0622 오후수업

--- 07_Spring/ex04/src/main/java/org/scoula/config/RootConfig.java
+++ 07_Spring/ex04/src/main/java/org/scoula/config/RootConfig.java
@@ -4,6 +4,7 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
@@ -22,6 +23,7 @@ import javax.sql.DataSource;
 
 @Configuration
 @PropertySource({"classpath:/application.properties"})
+@MapperScan(basePackages = {"org.scoula.mapper"}) // Mapper 인터페이스 스캔 설정
 public class RootConfig {
 
     @Autowired // DI
